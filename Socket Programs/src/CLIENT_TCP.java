eca2ded89428dc17a816ad3f6a271fb82ccea438
Sparta-NR <*#Github334*#>
2018-12-13 15:48:45 +0530

    Client Server Double a number TCP

--- Socket Programs/src/CLIENT_TCP.java
+++ Socket Programs/src/CLIENT_TCP.java
@@ -1,4 +1,28 @@
 
-public class CLIENT_TCP {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+public class CLIENT_TCP 
+{
+  public static void main(String ar[]) throws IOException
+  {
+	  int number,temp;
+	  
+	  Scanner sc = new Scanner(System.in);    //to take input from user
+	  Socket cl = new Socket("localhost",1236);   //creating socket
+	  Scanner sc1 = new Scanner(cl.getInputStream());  //take data from server
+	  
+	  System.out.print("Enter 5 numbers");
+	
+	  number = sc.nextInt();    
+	  PrintStream ob = new PrintStream(cl.getOutputStream()); //convert data to stream tp send to server
+	  ob.println(number);  //send data to server using print stream object
+	  
+	  temp = sc1.nextInt();
+	  System.out.println(temp);  
+	  
+	  cl.close();
+	  sc1.close();
+  } 
 }
--- Socket Programs/src/SERVER_TCP.java
+++ Socket Programs/src/SERVER_TCP.java
@@ -1,4 +1,25 @@
+//client server model to double a number using TCP
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class SERVER_TCP {
-
+public class SERVER_TCP 
+{
+  public static void main(String ar[]) throws IOException
+  {
+	  int number,temp;
+	 
+	  ServerSocket listen = new ServerSocket(1236);   //socket to listen to client
+	  System.out.print("Server is listening.....");
+	  Socket ss = listen.accept();  //accepts incoming requests
+	  
+	  Scanner sc = new Scanner(ss.getInputStream());  //accept data from client
+	  number = sc.nextInt();
+	  
+	  temp =2*number;
+	  PrintStream ob = new PrintStream(ss.getOutputStream()); //convert data to stream tp send to server
+	  ob.println(temp);
+	 
+	  ss.close();
+  }
 }
